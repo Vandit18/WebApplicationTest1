88f03aeb377e36b21df624d1a51d622d4632e46e
vandit18 <vanditkothari@gmail.com>
2016-05-17 13:13:19 -0400

    adding more pages

--- WebApplicationTest1/TestForm.aspx
+++ WebApplicationTest1/TestForm.aspx
@@ -2,14 +2,23 @@
 
 <!DOCTYPE html>
 
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head runat="server">
-    <title></title>
+<html xmlns="http://www.w3.org/1999/xhtml" >
+<head id="Head1" runat="server">
+    <title>Untitled Page</title>
 </head>
 <body>
     <form id="form1" runat="server">
     <div>
-        <h1>Welcome Vandit!!</h1>
+    <asp:Label ID="lb1" runat="server" Font-Bold="True" ForeColor="#FF3300"></asp:Label><br />
+     <asp:Label ID="Label2" runat="server" Text="Name" Font-Bold="True"
+            Width="100px" BackColor="#FFFF66" ForeColor="#FF3300"></asp:Label>
+        <asp:TextBox ID="TextBox_user_name" runat="server" ForeColor="#993300" Width="100px"></asp:TextBox><br />
+        <asp:Label ID="Label3" runat="server" Text="Password" Font-Bold="True"
+            Width="100px" BackColor="#FFFF66" ForeColor="#FF3300"></asp:Label>
+        <asp:TextBox ID="TextBox_password" runat="server" ForeColor="#CC6600"
+            TextMode="Password" Width="100px"></asp:TextBox><br />
+        <asp:Button ID="btn_login" runat="server" Text="Login" Font-Bold="True"
+            BackColor="#CCFF99" onclick="btn_login_Click"  /><br />
     </div>
     </form>
 </body>
--- WebApplicationTest1/TestForm.aspx.cs
+++ WebApplicationTest1/TestForm.aspx.cs
@@ -4,14 +4,43 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using MySql.Data.MySqlClient;
+using System.Data;
 
 namespace WebApplicationTest1
 {
     public partial class TestForm : System.Web.UI.Page
     {
+
+        string strcon = "Server=sql.computerstudi.es;Database=gc200299657;Uid=gc200299657;Pwd=p4^AJCpe";
+        string str;
+        MySqlCommand com;
+        object obj;
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            
+        }
 
+        protected void btn_login_Click(object sender, EventArgs e)
+        {
+            MySqlConnection con = new MySqlConnection(strcon);
+            con.Open();
+            str = "select count(*) from login where UserName=@UserName and Password =@Password";
+            com = new MySqlCommand(str, con);
+            com.CommandType = CommandType.Text;
+            com.Parameters.AddWithValue("@UserName", TextBox_user_name.Text);
+            com.Parameters.AddWithValue("@Password", TextBox_password.Text);
+            obj = com.ExecuteScalar();
+            if (Convert.ToInt32(obj) != 0)
+            {
+                Response.Redirect("Welcome.aspx");
+            }
+            else
+            {
+                lb1.Text = "invalid user name and password";
+            }
+            con.Close();
         }
     }
 }
